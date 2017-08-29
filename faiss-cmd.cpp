5966a9d2fd910e0c1db2d7a72d299ab59fb93ede
Hieu Hoang <hieuhoang@gmail.com>
2017-08-29 09:24:18 +0100

    c++ demo taken from tests/demo_ivfpq_indexing

---
+++ faiss-cmd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "IndexIVFPQ.h"
+#include "IndexFlat.h"
+#include "index_io.h"
+
+using namespace std;
+
+int main()
+{
+  cerr << "Starting..." << endl;
+
+  cerr << "Finished." << endl;
+  return 0;
+}
+
