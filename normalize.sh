a986cd7de9d60c0a5309799d65a6cd5b42b7202d
nuno <1140358@isep.ipp.pt>
2017-08-30 16:19:04 +0100

    normalized shell file

--- normalize.sh
+++ normalize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Normalize script
-#set -x
+# set -x
 function usage {
-    echo "usage: $0 <filename> <source> <target> <min> <max> <output>\n"
+    echo "usage: $(basename $0) <filename> <source> <target> <min> <max> <output>\n"
     echo "example: $0 eparl7.fr-es-en en es 1 50 output.txt"
     exit 1
 }
@@ -13,6 +13,7 @@ MOSES_LOWER_SCRIPT="$MOSES_PATH/scripts/tokenizer/lowercase.perl"
 MOSES_TOKENIZER_SCRIPT="$MOSES_PATH/scripts/tokenizer/tokenizer.perl"
 MOSES_CLEAN_CURPOS_SCRIPT="$MOSES_PATH/scripts/training/clean-corpus-n.perl"
 
+
 if [ ! -z ${MOSES_PROGRAM} ]; then
   MOSES_PATH=$MOSES_PROGRAM
 fi
@@ -21,6 +22,8 @@ if [ ! -d ${MOSES_PATH} ];then
    echo "Please set your MOSES_PROGRAM environment variable first!"
    exit
 fi
+
+
 
 if [ "$#" -ne 6 ];then
    usage
@@ -38,13 +41,15 @@ min=$4
 max=$5
 name_of_output_file=$6
 
-cmd=$(zcat ${name_of_input_file}.${name_of_target_language}.gz  \ |
-   sort | \
-   ${MOSES_LOWER_SCRIPT} | \
-   ${MOSES_TOKENIZER_SCRIPT} | \
-   ${MOSES_CLEAN_CURPOS_SCRIPT} ${name_of_source_language} ${name_of_target_language} ${min} ${max} ${name_of_output_file})
+zcat ${name_of_input_file}.${name_of_source_language}.gz | sort |${MOSES_LOWER_SCRIPT} > "./.lower.${name_of_source_language}"
+${MOSES_TOKENIZER_SCRIPT} < "./.lower.${name_of_source_language}" > "./.tokenized.${name_of_source_language}"
 
-echo $cmd
+zcat ${name_of_input_file}.${name_of_target_language}.gz | sort |${MOSES_LOWER_SCRIPT} > "./.lower.${name_of_target_language}"
+${MOSES_TOKENIZER_SCRIPT} < "./.lower.${name_of_target_language}" > "./.tokenized.${name_of_target_language}"
+
+
+${MOSES_CLEAN_CURPOS_SCRIPT} "./.tokenized" ${name_of_source_language} ${name_of_target_language} ${name_of_output_file} ${min} ${max}
+
 
 if [ $? -ne 0 ]; then
    echo "Something bad happened"
