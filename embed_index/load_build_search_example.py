2204ae9157a9ae307a6d7a7bf68898d8a9a17a51
Kostya Bazhanov <kostyabazhanov@mail.ru>
2017-08-31 13:15:22 +0000

    better

--- embed_index/load_build_search_example.py
+++ embed_index/load_build_search_example.py
@@ -5,32 +5,69 @@ from __future__ import print_function
 
 import argparse
 import faiss
+import gzip
+import itertools
+import sys
 import torch
 import torch.utils.serialization
-import sys
 
 
 def _parse_options():
     p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument(
         '--embeddings',
-        required=True,
+        default='/data/holger/europarl.un16lc/embed.th7/eparl7lu.efs.w50.en.00.efsraz-1allmd.BLSTM.1x512.max.@13.embed',
         help='embeddings file, e.g. "eparl7lu.efs.w50.en.00.efsraz-1allmd.BLSTM.1x512.max.@13.embed"')
+    p.add_argument(
+        '--sentences',
+        default='/data/holger/europarl.un16lc/data.norm.un6lc/eparl7lu.efs.w50.en.gz',
+        help=('sentences (order of the lines and embeddings is expected to be identical), e.g.' +
+              ' "eparl7lu.efs.w50.en.gz"'))
+    p.add_argument(
+        '--offset',
+        default=0,
+        help='embeddings file offset (0 for the example emddings file)')
+    p.add_argument(
+        '--sent-index',
+        default=100500,
+        help='sentence index to search for', )
+    p.add_argument(
+        '-k',
+        default=10,
+        help='number of nearest neighbors to search for', )
     return p.parse_args()
+
+
+def _open(path, mode):
+    if path.endswith('.gz'):
+        return gzip.open(path, mode)
+    return open(path, mode)
 
 
 def _main(a):
     t = torch.utils.serialization.load_lua(a.embeddings)
 
+    print('Loading sentences...')
+    sys.stdout.flush()
+    sent = _open(a.sentences, 'rb').readlines()
+
     res = faiss.StandardGpuResources()
     index = faiss.GpuIndexFlatIP(res, t.size()[1])
-    print('Building index...')
+    print('Building index on GPU...')
     sys.stdout.flush()
     index.add(x=t.numpy())
 
     print('Searching index...')
     sys.stdout.flush()
-    print(index.search(x=t[7:8].numpy(), k=10))
+    distances, indices = index.search(x=t[a.sent_index:a.sent_index + 1].numpy(), k=10)
+
+    print('Searched for: "{}"'.format(sent[a.sent_index]))
+    print('Nearest neighbors (distance \\t index \\t sentence):')
+    for d, i in itertools.izip(distances[0], indices[0]):
+        j = i + a.offset
+        c_sentence = sent[j].encode('string-escape')
+        print('{dist:.2f}\t{index}\t"{sentence}"'.format(dist=d, index=j, sentence=c_sentence))
+    sys.stdout.flush()
 
     print('Done')
     sys.stdout.flush()
